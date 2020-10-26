ca9b782e64264a26affa56955b9c0d307c5e7c55
pokmon2499 <73430030+pokmon2499@users.noreply.github.com>
2020-10-26 12:35:21 +0700

    Update update.php

--- update.php
+++ update.php
@@ -2,7 +2,7 @@
 //เวอร์ชั่นไว้สำหรับอัพเดทหากมีการอัพเดทต้องเปลี่ยนตัวเลขให้เยอะกว่าเดิม
 "Version":"7.1",
 //ลิ้งค์สำหรับอัพเดทเซิร์ฟเวอร์
-"Url":"https://github.com/pokmon2499/2020/raw/main/2020.zip" ,
+"Url":"https://github.com/pokmon2499/2020/raw/main/vip.zip" ,
 "Changelog":"
 พบไฟล์ใหม่ (28/05/2020)
 รองรับ ซิม TRUE & DTAC
